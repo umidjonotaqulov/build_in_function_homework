e2b2ed144355771bf4f23f179ff0462252c0b7b9
umidjonotaqulov <otaqulovumidjon2003@gmail.com>
2022-07-11 18:33:09 +0500

    x10

--- build_func10.py
+++ build_func10.py
@@ -1,2 +1,17 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func10
+#"x" o'zgaruvchisini yarating va unga 2 qiymatini belgilang.
+#"y" o'zgaruvchisini yarating va unga 4 qiymatini belgilang.
+#Qiymatni hisoblang: .
+#Natijani "javob"ga tayinlang
+#Natijani 2 kasrgacha yaxlitlang.
+#Pow funksiyasidan foydalaning
+#Dumaloq funksiyadan foydalaning
+
+x=2
+
+y=4
+
+answer=round((3*pow(y,1/2)+pow(x,2/3)),2)
+
+print(answer)

975766906f5835fd9e824bb69246af54fd4e5836
umidjonotaqulov <otaqulovumidjon2003@gmail.com>
2022-07-11 18:15:34 +0500

    x5

--- build_func05.py
+++ build_func05.py
@@ -1,2 +1,10 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func05
+
+n=3
+
+x=6
+
+answer=pow(x,n)+pow(n,x)
+
+print(answer)

7d34e7be5aef71937360ba2651df68657c20f290
umidjonotaqulov <otaqulovumidjon2003@gmail.com>
2022-07-11 18:17:39 +0500

    x7

--- build_func07.py
+++ build_func07.py
@@ -1,2 +1,10 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func07
+
+x=5
+
+y=2
+
+answer=pow(x,2)+6*pow(x,3)+3*x*y
+
+print((answer))

8eb18bb6df4bf38fadb70a47ff06542b55c9a7c8
umidjonotaqulov <otaqulovumidjon2003@gmail.com>
2022-07-11 18:18:11 +0500

    x8

--- build_func08.py
+++ build_func08.py
@@ -1,2 +1,10 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func08
+
+x=7
+
+y=1
+
+answer=5*pow(x,2)*pow(y,3)+x*pow(y,2)
+
+print(answer)
